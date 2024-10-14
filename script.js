c134c82ee1016a4660aad8fea9fe5a858eb218d8
suryakantkashyap <suryakantkashyap692@gmail.com>
2024-10-14 12:27:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,35 @@ function lps(s, lo, hi, memo) {
     return memo[lo][hi];
 }
 
+function constructLPS(s, lo, hi, memo) {
+    let lpsStr = "";
+    
+    while (lo <= hi) {
+        if (s[lo] === s[hi]) {
+            lpsStr += s[lo];
+            lo++;
+            hi--;
+        } else if (memo[lo][hi - 1] > memo[lo + 1][hi]) {
+            hi--;
+        } else {
+            lo++;
+        }
+    }
+
+    // Construct the full LPS by adding the reverse of the first half
+    const secondHalf = lpsStr.split('').reverse().join('');
+    return lpsStr + secondHalf.slice(1); // Avoid duplicating the middle character
+}
+
 document.getElementById('calculateButton').addEventListener('click', () => {
     const userInput = document.getElementById('inputString').value;
     const n = userInput.length;
     const memo = Array.from({ length: n }, () => Array(n));
-    const lengthOfLPS = lps(userInput, 0, n - 1, memo);
     
-    document.getElementById('result').textContent = "The length of the LPS is " + lengthOfLPS;
+    const lengthOfLPS = lps(userInput, 0, n - 1, memo);
+    const lpsString = constructLPS(userInput, 0, n - 1, memo);
+    
+    document.getElementById('result').textContent = 
+        "The length of the LPS is " + lengthOfLPS + 
+        " and the LPS is: " + lpsString;
 });
